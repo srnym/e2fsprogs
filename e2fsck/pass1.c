2b80111ffc0a8ea9f071a5f726d8811d22c22213
Saranya Muruganandam <saranyamohan@google.com>
2020-12-23 09:19:50 -0800

    Fix compilation issues when merging wangs and teds changes
    Does  not include the pthreads change from Ted

--- e2fsck/pass1.c
+++ e2fsck/pass1.c
@@ -3205,6 +3205,33 @@ out:
 	return NULL;
 }
 
+
+dgrp_t ext2fs_get_avg_group(ext2_filsys fs)
+{
+	dgrp_t average_group;
+	unsigned flexbg_size;
+
+	if (fs->fs_num_threads <= 1)
+		return fs->group_desc_count;
+
+	average_group = fs->group_desc_count / fs->fs_num_threads;
+	if (average_group <= 1)
+		return 1;
+
+	if (ext2fs_has_feature_flex_bg(fs->super)) {
+		int times = 1;
+
+		flexbg_size = 1 << fs->super->s_log_groups_per_flex;
+		if (average_group % flexbg_size) {
+			times = average_group / flexbg_size;
+			average_group = times * flexbg_size;
+		}
+	}
+
+	return average_group;
+}
+
+
 static int e2fsck_pass1_threads_start(struct e2fsck_thread_info **pinfo,
 				      e2fsck_t global_ctx)
 {
@@ -3241,6 +3268,7 @@ static int e2fsck_pass1_threads_start(struct e2fsck_thread_info **pinfo,
 	}
 
 	average_group = ext2fs_get_avg_group(global_ctx->fs);
+
 	for (i = 0; i < num_threads; i++) {
 		tmp_pinfo = &infos[i];
 		tmp_pinfo->eti_thread_index = i;
